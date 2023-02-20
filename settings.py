a4e562ab659195e5cfdbfdf7c2704bd93e5c9325
Keith Bates <bateskeith@pm.me>
2023-02-20 04:06:21 -0700

    Changed bg color to cardtable green

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ class Settings:
 
     def __init__(self):
         self.screen_width, self.screen_height = 1200, 800
-        self.bg_color = (225, 225, 225)
+        self.bg_color = (0, 81, 44)
         
